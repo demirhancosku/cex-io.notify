7724ea27bb16642ab8451872a0aed8c1e40ea2e4
coskudemirhan <cosku@dokuziki.com>
2017-07-04 20:06:17 +0300

    multiple resource support

--- App/Trader.js
+++ App/Trader.js
@@ -13,7 +13,7 @@ var sellProfitMargin = 0.1;
 var debug = true;
 var bot = {};
 
-var forcast = function (resource) {
+var forcast = function (resources) {
     db.query('SELECT * FROM prices ORDER BY id DESC LIMIT 300', function (err, rows) {
 
 
@@ -50,78 +50,96 @@ var forcast = function (resource) {
                 bidForecast -= tBid.data[forcastCount - i][1] * Bidcoeffs[i];
             }
 
-            if(debug){
+            if (debug) {
                 console.log('Alış Fiyatı: ' + lastAskPrices[lastAskPrices.length - 1][1]);
                 console.log('Tahmini Alış Fiyatı: ' + askForecast);
                 console.log('Ortalama Alış Fiyatı: ' + tAsk.mean());
             }
 
-            var suitableForAsk = false;
-            if (resource.ask === null) {
-                /*
-                 if(parseFloat(lastAskPrices[lastAskPrices.length-1][1])  < tAsk.mean()){
-                 if(parseFloat(askForecast) > parseFloat(lastAskPrices[lastAskPrices.length-1][1])){
+            for (r in resources) {
 
-                 if((parseFloat(resource.bid) + parseFloat(buyProfitMargin)) < (parseFloat(lastAskPrices[lastAskPrices.length-1][1]))){
+                var resource = resources[r];
 
-                 suitableForAsk = true;
-                 buyKnow(resource,lastAskPrices[lastAskPrices.length-1][1]);
-                 }
+                var suitableForAsk = false;
+                if (resource.ask === null) {
 
-                 }
+                    if (parseFloat(lastAskPrices[lastAskPrices.length - 1][1]) < tAsk.mean()) {
+                        if (parseFloat(askForecast) > parseFloat(lastAskPrices[lastAskPrices.length - 1][1])) {
 
-                 }
-                 */
-                if ((parseFloat(resource.bid) + parseFloat(buyProfitMargin)) < (parseFloat(lastAskPrices[lastAskPrices.length - 1][1]))) {
+                            if ((parseFloat(resource.bid) + parseFloat(buyProfitMargin)) < (parseFloat(lastAskPrices[lastAskPrices.length - 1][1]))) {
 
-                    suitableForAsk = true;
-                    buyKnow(resource, lastAskPrices[lastAskPrices.length - 1][1]);
+                                suitableForAsk = true;
+                                buyKnow(resource, lastAskPrices[lastAskPrices.length - 1][1]);
+                            }
+
+                        }
+
+                    }
+
+                    /*
+                     if ((parseFloat(resource.bid) + parseFloat(buyProfitMargin)) < (parseFloat(lastAskPrices[lastAskPrices.length - 1][1]))) {
+
+                     suitableForAsk = true;
+                     buyKnow(resource, lastAskPrices[lastAskPrices.length - 1][1]);
+                     }
+                     */
+                    if (debug)
+                        console.log(resource.bid + ' için alış istediğimiz değer: ' + (parseFloat(resource.bid) - parseFloat(buyProfitMargin)));
+
+
+                    if (debug)
+                        console.log(resource.bid + ' ile sattığımız alış için uygun mu: ' + suitableForAsk);
                 }
-                if(debug)
-                    console.log('Alış İstediğimiz Değer: ' + (parseFloat(resource.bid) - parseFloat(buyProfitMargin)));
+
             }
 
-            if(debug){
-                console.log('Alış İçin Uygun mu: ' + suitableForAsk + '\n');
 
-
+            if (debug) {
+                console.log('\n');
                 console.log('Satış Fiyatı: ' + lastBidPrices[lastBidPrices.length - 1][1]);
                 console.log('Tahmini Satış Fiyatı: ' + bidForecast);
                 console.log('Ortalama Satış Fiyatı: ' + tBid.mean());
             }
 
-            var suitableForBid = false;
-            if (resource.bid === null) {
-                /*
-                 if(lastBidPrices[lastBidPrices.length-1][1] > tBid.mean()){
 
-                 if(bidForecast < lastBidPrices[lastBidPrices.length-1][1]){
+            for (t in resources) {
+
+                var resource = resources[t];
+                var suitableForBid = false;
+                if (resource.bid === null) {
+
+                    if (lastBidPrices[lastBidPrices.length - 1][1] > tBid.mean()) {
+
+                        if (bidForecast < lastBidPrices[lastBidPrices.length - 1][1]) {
 
 
-                 if((parseFloat(resource.ask) + parseFloat(sellProfitMargin) ) <  parseFloat(lastBidPrices[lastBidPrices.length-1][1])){
+                            if ((parseFloat(resource.ask) + parseFloat(sellProfitMargin) ) < parseFloat(lastBidPrices[lastBidPrices.length - 1][1])) {
 
-                 suitableForBid = true;
-                 sellKnow(resource, lastBidPrices[lastBidPrices.length-1][1]);
-                 }
-                 }
+                                suitableForBid = true;
+                                sellKnow(resource, lastBidPrices[lastBidPrices.length - 1][1]);
+                            }
+                        }
 
-                 }
-                 */
-                if ((parseFloat(resource.ask) + parseFloat(sellProfitMargin) ) < parseFloat(lastBidPrices[lastBidPrices.length - 1][1])) {
+                    }
 
-                    suitableForBid = true;
-                    sellKnow(resource, lastBidPrices[lastBidPrices.length - 1][1]);
+                    /*if ((parseFloat(resource.ask) + parseFloat(sellProfitMargin) ) < parseFloat(lastBidPrices[lastBidPrices.length - 1][1])) {
+
+                     suitableForBid = true;
+                     sellKnow(resource, lastBidPrices[lastBidPrices.length - 1][1]);
+                     }*/
+
+                    if (debug) {
+                        console.log(resource.ask + ' için satış istediğimiz değer: ' + ( parseFloat(resource.ask) + parseFloat(sellProfitMargin) ));
+                        console.log(resource.ask + ' ile aldığımız satış için uygun mu: ' + suitableForBid + '\n');
+                    }
                 }
-                if(debug)
-                    console.log('Satış İstediğimiz Değer: ' + ( parseFloat(resource.ask) + parseFloat(sellProfitMargin) ));
-            }
 
-            if(debug) {
+                if (debug) {
 
-                console.log('Satış İçin Uygun mu: ' + suitableForBid + '\n');
+                    console.log('\n');
+                    console.log('\n');
+                }
 
-
-                console.log('\n');
             }
 
         }
@@ -130,15 +148,15 @@ var forcast = function (resource) {
 }
 
 
-var init = function (client,chatBot) {
+var init = function (client, chatBot) {
     bot = chatBot;
     setInterval(function () {
         fs.readFile('resources.json', 'utf8', function readFileCallback(err, data) {
             if (err) {
                 console.log(err);
             } else {
-                var resource = JSON.parse(data);
-                forcast(resource);
+                var resources = JSON.parse(data);
+                forcast(resources);
             }
         });
     }, 5000);
@@ -164,33 +182,60 @@ var init = function (client,chatBot) {
 }
 
 var buyKnow = function (resource, ask) {
-    bot.sendMessage(22353916, ask +'$ değerinde ' + resource.amount + ' ETH Satın Aldım');
-    resource.ask = ask;
-    resource.bid = null;
-    fs.writeFile('resources.json', JSON.stringify(resource), 'utf8', function () {
-        db.query("INSERT INTO market_logs VALUES(null,@type,'@value','@amount',@date)", {
-            type: 'buy',
-            value: ask,
-            amount: resource.amount,
-            date: +new Date()
-        });
-    });
+    bot.sendMessage(22353916, ask + '$ değerinde ' + resource.amount + ' ETH Satın Aldım');
 
+
+    fs.readFile('resources.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var resources = JSON.parse(data);
+
+            for (i in resources) {
+                if (resources[i].bid === resource.bid) {
+                    resources[i].ask = ask;
+                    resources[i].bid = null;
+                }
+            }
+
+            fs.writeFile('resources.json', JSON.stringify(resources), 'utf8', function () {
+                db.query("INSERT INTO market_logs VALUES(null,@type,'@value','@amount',@date)", {
+                    type: 'buy',
+                    value: ask,
+                    amount: resource.amount,
+                    date: +new Date()
+                });
+            });
+        }
+    });
 
 }
 
 var sellKnow = function (resource, bid) {
-    bot.sendMessage(22353916, bid +'$ değerinde ' + resource.amount + ' ETH Sattım');
+    bot.sendMessage(22353916, bid + '$ değerinde ' + resource.amount + ' ETH Sattım');
 
-    resource.ask = null;
-    resource.bid = bid;
-    fs.writeFile('resources.json', JSON.stringify(resource), 'utf8', function () {
-        db.query("INSERT INTO market_logs VALUES(null,@type ,'@value','@amount',@date)", {
-            type: 'sell',
-            value: bid,
-            amount: resource.amount,
-            date: +new Date()
-        });
+    fs.readFile('resources.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var resources = JSON.parse(data);
+
+            for (i in resources) {
+                if (resources[i].bid === resource.bid) {
+                    resources[i].ask = null;
+                    resources[i].bid = bid;
+                }
+            }
+
+            fs.writeFile('resources.json', JSON.stringify(resources), 'utf8', function () {
+                db.query("INSERT INTO market_logs VALUES(null,@type ,'@value','@amount',@date)", {
+                    type: 'sell',
+                    value: bid,
+                    amount: resource.amount,
+                    date: +new Date()
+                });
+            });
+        }
     });
 
 
